af6ae77d16816551183df97fe71b330d9df27663
Katherine Gonchar <katherinegon4ar@gmail.com>
2022-06-19 18:25:03 +0300

    Added units conversion and weather icon

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ let currentTemp = document.querySelector("#temperature");
 let weatherDescription = document.querySelector("#weather-description");
 let humidity = document.querySelector("#humidity");
 let wind = document.querySelector("#wind");
+let weatherIcon = document.querySelector(".weather-icon");
+let weatherIconCode;
+let weatherIconSrc;
+let celsiusTemp;
+
 
 let apiKey = "819d2830a6cec8ee668b535af2021283";
 let apiURL = "https://api.openweathermap.org/data/2.5/weather?";
@@ -41,9 +46,14 @@ function changeCity(event) {
 function changeInfo(response) {
   city.innerHTML = response.data.name;
   currentTemp.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemp = Math.round(response.data.main.temp);
   weatherDescription.innerHTML = response.data.weather[0].main;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   wind.innerHTML = `Wind: ${response.data.wind.speed} m/s`;
+  weatherIconCode = response.data.weather[0].icon;
+  weatherIconSrc = `http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
+  weatherIcon.setAttribute("src", `${weatherIconSrc}`);
+  console.log(response);
 }
 
 let buttonCurrent = document.querySelector("#current-location-button");
@@ -57,4 +67,22 @@ function locatePosition() {
       .get(`${apiURL}lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`)
       .then(changeInfo);
   }
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", changeToF);
+function changeToF(){
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  currentTemp.innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", changeToC);
+function changeToC(){
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  currentTemp.innerHTML = celsiusTemp;
 }
