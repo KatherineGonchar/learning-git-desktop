03906c8cb3d79af47891a3e5b4489eb3861d9946
Katherine Gonchar <katherinegon4ar@gmail.com>
2022-06-26 20:00:48 +0300

    Deleted units conversion, added daily forecast for the next five days

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ let wind = document.querySelector("#wind");
 let weatherIcon = document.querySelector(".weather-icon");
 let weatherIconCode;
 let weatherIconSrc;
-let celsiusTemp;
 
 
 let apiKey = "819d2830a6cec8ee668b535af2021283";
@@ -45,15 +44,14 @@ function changeCity(event) {
 
 function changeInfo(response) {
   city.innerHTML = response.data.name;
-  currentTemp.innerHTML = Math.round(response.data.main.temp);
-  celsiusTemp = Math.round(response.data.main.temp);
+  currentTemp.innerHTML = `${Math.round(response.data.main.temp)}`;
   weatherDescription.innerHTML = response.data.weather[0].main;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   wind.innerHTML = `Wind: ${response.data.wind.speed} m/s`;
   weatherIconCode = response.data.weather[0].icon;
-  weatherIconSrc = `http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
+  weatherIconSrc = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
   weatherIcon.setAttribute("src", `${weatherIconSrc}`);
-  console.log(response);
+  getForecast(response.data.coord);
 }
 
 let buttonCurrent = document.querySelector("#current-location-button");
@@ -69,20 +67,41 @@ function locatePosition() {
   }
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", changeToF);
-function changeToF(){
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  currentTemp.innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
+function displayForecast(response){
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let forecast = response.data.daily;
+  forecast.forEach(function (forecastDay, index){
+    if(index<6){
+      forecastHTML = forecastHTML + `
+            <div class="col-2">
+              <div class="weather-forecast-date">
+                ${formatDay(forecastDay.dt)}
+              </div>
+              <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="forecast icon for today" height="50px">
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span>
+                <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
+              </div>
+            </div>`;
+    }
+
+  })
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", changeToC);
-function changeToC(){
-  event.preventDefault();
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-  currentTemp.innerHTML = celsiusTemp;
+function getForecast(coordinates){
+  console.log(coordinates);
+  let currentLat = coordinates.lat;
+  let currentLong = coordinates.lon;
+  let forecastApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentLat}&lon=${currentLong}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
+  axios.get(forecastApiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp){
+  let date = new Date(timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
